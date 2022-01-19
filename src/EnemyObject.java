54a65a4eb3f65ceb5f99f1ad98eefc49eb3643c5
MinJi Kim <dev.kimminji@gmail.com>
2022-01-19 16:04:53 +0900

    Update EnemyObject.java

--- src/EnemyObject.java
+++ src/EnemyObject.java
@@ -1,10 +1,6 @@
 public class EnemyObject extends GameObject{
-    int enemyPosX;
-    int enemyPosY;
 
     EnemyObject(int posX, int posY, String image) {
         super(posX, posY, image);
-        this.enemyPosX = posX;
-        this.enemyPosY = posY;
     }
 }
